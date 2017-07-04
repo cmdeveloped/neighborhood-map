7bbb8526b97a78c31e8bce88afdddcebc68bc597
cmdeveloped <cmwebdevs@gmail.com>
2017-07-03 19:15:00 -0500

    Add different styling to markers.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 var map;
-var initialIcon;
-var focusIcon;
-var infoWindow;
+var markers = [];
 
 function throwError() {
   alert('Could not load page properly.');
@@ -10,74 +8,185 @@ function throwError() {
 function initMap() {
   var styles = [
     {
-      featureType: 'water',
-      stylers: [
-        {
-          color: '#b2ebf2'
-        }
-      ]
+       "featureType": "poi",
+       "elementType": "all",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#b2ebf2"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#c8d7d4"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#070707"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
     }
-  ];
+];
 
+  var initialIcon = makeMarkerIcon('455a64');
+  var focusIcon = makeMarkerIcon('b2ebf2');
   var home = {lat: 35.9372555, lng: -86.8104977};
+
   var mapOptions = {
     center: home,
-    zoom: 15,
+    zoom: 13,
     styles: styles,
+    mapTypeControl: false
   }
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
   var locations = [
     {
-      name: 'Tupelo Honey',
-      lat: 35.9516141,
-      lng: -86.8100217,
+      title: 'Tupelo Honey',
+      LatLng: {
+        lat: 35.9516098,
+        lng: -86.807833,
+      }
     },
     {
-      name: 'The Draft Room at Kings',
-      lat: 35.9543433,
-      lng: -86.8170027,
+      title: 'The Draft Room at Kings',
+      LatLng: {
+        lat: 35.954339,
+        lng: -86.814814,
+      }
     },
     {
-      name: 'Jonathan\'s Grille',
-      lat: 35.9525577,
-      lng: -86.8167662,
+      title: 'Jonathan\'s Grille',
+      LatLng: {
+        lat: 35.9525534,
+        lng: -86.8145775,
+      }
     },
     {
-      name: 'Kona Grill',
-      lat: 35.9547463,
-      lng: -86.8175827,
+      title: 'Kona Grill',
+      LatLng: {
+        lat: 35.954742,
+        lng: -86.815394,
+      }
     },
     {
-      name: 'Connors Steak & Seafood',
-      lat: 35.9550245,
-      lng: -86.8173964,
+      title: 'Connors Steak & Seafood',
+      LatLng: {
+        lat: 35.9550202,
+        lng: -86.8152077,
+      }
     },
     {
-      name: 'Brewhouse South',
-      lat: 35.9531633,
-      lng: -86.8154846,
+      title: 'Brewhouse South',
+      LatLng: {
+        lat: 35.953159,
+        lng: -86.8132959,
+      }
     },
     {
-      name: 'John\'s Burgers',
-      lat: 35.9560944,
-      lng: -86.8167678,
+      title: 'John\'s Burgers',
+      LatLng: {
+        lat: 35.9560901,
+        lng: -86.8145791,
+      }
     },
     {
-      name: 'Noodles and Company',
-      lat: 35.9507806,
-      lng: -86.8098181,
+      title: 'Noodles and Company',
+      LatLng: {
+        lat: 35.9507763,
+        lng: -86.8076294
+      }
     },
     {
-      name: 'Swanky\'s Taco Shop',
-      lat: 35.9503513,
-      lng: -86.8099877,
+      title: 'Swanky\'s Taco Shop',
+      LatLng: {
+        lat: 35.950347,
+        lng: -86.807799
+      }
     },
     {
-      name: 'L&L Hawaiian Grill',
-      lat: 35.9517293,
-      lng: -86.8098397,
+      title: 'L&L Hawaiian Grill',
+      LatLng: {
+        lat: 35.951725,
+        lng: -86.807651
+      }
     }
+  ];
 
-  ]
+  var infoWindow = new google.maps.InfoWindow();
+  var bounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < locations.length; i++) {
+    var position = locations[i].LatLng;
+    var title = locations[i].title;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      icon: initialIcon,
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+    markers.push(marker);
+    bounds.extend(marker.position);
+    marker.addListener('click', function() {
+      populateInfoWindow(this, infoWindow);
+    });
+    marker.addListener('mouseover', function() {
+      this.setIcon(focusIcon);
+    });
+    marker.addListener('mouseout', function() {
+      this.setIcon(initialIcon);
+    });
+  }
+  map.fitBounds(bounds);
+
+  function populateInfoWindow(marker, infowindow) {
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('<div>' + marker.title + '</div>');
+      infowindow.open(map, marker);
+      infowindow.addListener('closeclick', function() {
+        infowindow.setMarker(null);
+      });
+    }
+  }
+  function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21,34));
+    return markerImage;
+  }
 }
